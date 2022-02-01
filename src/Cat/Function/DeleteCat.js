022b466bb115d8939af7d7fa2924e7e8cf53fcc7
polina-d <98765718+polina-d@users.noreply.github.com>
2022-02-02 01:59:59 +0300

    Delete DeleteCat.js

--- src/Cat/Function/DeleteCat.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function DeleteCat(props) {
-    return(
-        fetch(`https://internship.apps.robotbull.com/cats/delete_cat/${props.items.id}`,
-       {method: 'DELETE',
-       headers: 'id' })
-    )
-}
